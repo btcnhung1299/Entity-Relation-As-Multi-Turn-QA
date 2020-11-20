760fd57b7787e606f27317fb8be98f907e83432a
Khanh <duykhanhnguyen360@gmail.com>
2020-11-20 13:55:08 +0700

    change question template

--- data/query_templates/generate_all_questions.py
+++ data/query_templates/generate_all_questions.py
@@ -12,13 +12,12 @@ zalo_entity_names = [
     "cầu thủ",
     "đội bóng",
     "mốc thời gian", 
-    "tỉ số"
+    "điểm số"
 ]
 
 zalo_relation_tags = [
     "COMP",  # (CLU, CLU) compete with
-    "DEFE",  # (CLU, CLU) defeat / win over
-    "DRAW",  # (CLU, CLU) draw 
+    "SCOC",  # (CLU, SCO) have score
     "SCOP",  # (CLU, PER) score player
     "SCOT",  # (PER, TME) score time
     "CARP",  # (CLU, PER) card player,
@@ -29,8 +28,7 @@ zalo_relation_tags = [
 
 zalo_relations_names = [
     "đấu với",
-    "bị đánh bại bởi",
-    "hòa với",
+    "đạt được của",
     "ghi bàn cho",
     "là thời điểm ghi bàn của",
     "nhận thẻ phạt thuộc",
@@ -44,7 +42,7 @@ if __name__ == "__main__":
     templates = {"qa_turn1":{},"qa_turn2":{}}
     
     for head_entity, head_entity_name in zip(zalo_entity_tags, zalo_entity_names):
-        templates['qa_turn1'][head_entity] = "liệt kê tất cả {} trong bài báo.".format(head_entity_name)
+        templates['qa_turn1'][head_entity] = "liệt kê tất cả {}.".format(head_entity_name)
         
         for relation, relation_name in zip(zalo_relation_tags, zalo_relations_names):
             for tail_entity, tail_entity_name in zip(zalo_entity_tags, zalo_entity_names):
@@ -52,5 +50,5 @@ if __name__ == "__main__":
                     "liệt kê {} {} {} XXX.".format(tail_entity_name, relation_name, head_entity_name)
                 
     with open("zalo_all.json", "w") as f:
-        json.dump(templates, f)
+        json.dump(templates, f, ensure_ascii=False)
                 
